e1b6a263a09ada2085c00eac018fe7a6e4e2680a
Parker Lacy <parkerlacy17@gmail.com>
2024-12-01 13:40:22 -0700

    Day 1 part 1 complete

---
+++ solutions/day1.go
@@ -0,0 +1,59 @@
+// Pair the smallest # in the left list with the smallest # in the right list and continue until all numbers are paired
+// Calculate distance between pair & add up all distances as we go
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	left, right := readInput()
+
+	fmt.Println(getTotalDistance(left, right))
+}
+
+func readInput() ([]int, []int) {
+	file, err := os.Open("../data/day1input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close() // close file when done, executes after the rest of the parent function ends
+
+	scanner := bufio.NewScanner(file)
+	var left, right []int
+
+	for scanner.Scan() {
+		var l, r int
+		fmt.Sscanf(scanner.Text(), "%d %d", &l, &r)
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return left, right
+}
+
+func getTotalDistance(left []int, right []int) int {
+	sort.Ints(left)
+	sort.Ints(right)
+
+	total := 0
+	for i := 0; i < len(left); i++ {
+		total += abs(left[i] - right[i])
+	}
+	return total
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
