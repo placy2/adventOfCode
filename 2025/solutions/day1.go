9292552aa352c88a4707d1db5f001e132ac3ae60
Parker Lacy <parkerlacy17@gmail.com>
2025-12-01 09:07:11 -0700

    Star 2 day 1 solution

--- 2025/solutions/day1.go
+++ 2025/solutions/day1.go
@@ -13,7 +13,7 @@ func main() {
 	// Read input data - if efficiency is needed do processing in same pass
 	report := readInput()
 	
-	fmt.Println("Number of times at position 0: ")
+	fmt.Println("Number of times passing position 0: ")
 	fmt.Println(countZeroes(report))
 }
 
@@ -42,40 +42,63 @@ func countZeroes(data []string) int {
 	total := 0
 	currentPos := 50
 	for _, line := range data {
-		currentPos = processTurn(line, currentPos)
+		currentPos, total = processTurn(line, currentPos, total)
 		if currentPos == 0 {
-			total++
+			//fmt.Println("Landed on position 0!", currentPos)
+			total += 1
 		}
 	}
 	return total
 }
 
 // Takes a single instruction string and returns the position after the move is performed as an int
-func processTurn(instruction string, currentPos int) int {
-	fmt.Println("Processing instruction:", instruction, "from position:", currentPos)
+// part 2: add arg to track total zeroes and increment and print when found
+func processTurn(instruction string, currentPos int, zeroCount int) (int, int) {
+	//fmt.Println("Processing instruction:", instruction, "from position:", currentPos)
 	// Extract the direction char
 	direction := instruction[0]
 
-	// Extract the number of steps, modulus 100 since the track is circular
+	// Extract the number of steps
 	steps, err := strconv.Atoi(instruction[1:])
 	if err != nil {
 		panic(err)
 	}
-	steps = steps % 100
 
-	if direction == 'L' {
-		currentPos -= steps
-
-		if currentPos < 0 {
-			currentPos += 100
-		}
-	} else if direction == 'R' {
-		currentPos += steps
-
-		if currentPos > 99 {
-			currentPos -= 100
-		}
+	// Check for > 100 steps
+	if steps / 100 > 0 {
+    //fmt.Println("Warning: instruction with more than 100 steps:", instruction)
+		zeroCount += steps / 100
 	}
 
-	return currentPos
+	// Modulus for circular array of 100 positions, can be done regardless
+	steps = steps % 100	
+
+	if direction == 'L' {
+		newPos := currentPos - steps
+		//fmt.Println("New position before wrap check:", newPos)
+
+		if newPos < 0 {
+			newPos += 100
+			// Only count wrap if we didn't end up exactly on 0
+			if currentPos != 0 && newPos != 0{
+				//fmt.Println("Wrapping around counterclockwise")
+				zeroCount += 1
+			}
+		}
+		currentPos = newPos
+	} else if direction == 'R' {
+		newPos := currentPos + steps
+
+		if newPos > 99 {
+			newPos -= 100
+			// Only count wrap if we didn't end up exactly on 0
+			if currentPos != 0 && newPos != 0 {
+				//fmt.Println("Wrapping around clockwise")
+				zeroCount += 1
+			}
+		}
+		currentPos = newPos
+	}
+
+	return currentPos, zeroCount
 }
